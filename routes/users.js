6cb1347caed6c65f4ddaa42ec596c0c5c6b11acc
rianpediaa <126028334+rianpediaa@users.noreply.github.com>
2023-03-14 15:42:53 +0700

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+module.exports = app => {
+  const Kontrol = require('../monggodb/controller')
+  const router = require('express').Router()
+  
+  router.get('/', Kontrol.findAll)
+  router.get('/:id', Kontrol.show)
+  router.post('/', Kontrol.create)
+  router.put('/:id', Kontrol.update)
+  router.delete('/:id', Kontrol.delete)
+  
+  app.use('/users',router)
+}
